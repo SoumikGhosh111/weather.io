814c296c31674ec801864d3cf1cb6221b56b4664
SoumikGhosh111 <132730107+SoumikGhosh111@users.noreply.github.com>
2024-02-13 15:26:40 +0530

    Update airQualityApi.js

--- src/functions/airQualityApi.js
+++ src/functions/airQualityApi.js
@@ -1,4 +1,4 @@
-export const AIR_QUALITY_API_URL = 'http://api.openweathermap.org/data/2.5/air_pollution?'
+export const AIR_QUALITY_API_URL = 'https://api.openweathermap.org/data/2.5/air_pollution?'
 // 39aa17207abc56cde0ebf9682f26eb30
 
 export const AIR_QUALITY_API_KEY = '39aa17207abc56cde0ebf9682f26eb30'; 
