66c5091954805318acc4f418ae91cac2f06392c0
SoumikGhosh111 <132730107+SoumikGhosh111@users.noreply.github.com>
2024-02-13 15:27:34 +0530

    Update fiveDaysForeCast.js

--- src/functions/fiveDaysForeCast.js
+++ src/functions/fiveDaysForeCast.js
@@ -1,4 +1,4 @@
-export const FiveDaysForeCast_API_URL = 'http://api.openweathermap.org/data/2.5/forecast'; 
+export const FiveDaysForeCast_API_URL = 'https://api.openweathermap.org/data/2.5/forecast'; 
 export const FiveDaysForeCast_API_KEY = '39aa17207abc56cde0ebf9682f26eb30';  
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
